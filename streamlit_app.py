b4f1baacc9a26b57a250a5a65866e438407bfca7
Sean Smart <117030053+Sean-Smart@users.noreply.github.com>
2023-03-09 17:25:21 +0000

    Update streamlit_app.py
    Added features to the menu. Decided to put a """fun""" spin on it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('Snowflake Diner')
+streamlit.header('Food made by Sean, For Sean (out of Sean)')
+streamlit.header('Menu')
+streamlit.text('Beans on toast (egg optional)')
+streamlit.text('Boring healthy cereal (ask for alternative milks)')
+streamlit.text('Nothing (fasting day)')
