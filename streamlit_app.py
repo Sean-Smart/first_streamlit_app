d87d2c5d15c8fd986722de4fb7bb31075d8596ea
Sean Smart <117030053+Sean-Smart@users.noreply.github.com>
2023-03-10 09:07:21 +0000

    Update streamlit_app.py
    Expanded menu, added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit
 streamlit.title('Snowflake Diner')
+
 streamlit.header('Food made by Sean, For Sean (out of Sean)')
 streamlit.header('Menu')
-streamlit.text('Beans on toast (egg optional)')
-streamlit.text('Boring healthy cereal (ask for alternative milks)')
-streamlit.text('Nothing (fasting day)')
+
+streamlit.text('🐔 Beans on toast (egg optional)')
+streamlit.text('🥣Boring healthy cereal (ask for alternative milks)')
+streamlit.text('🥗Nothing (fasting day)')
+streamlit.text('🥑🍞Avacado on toast')
+ 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
