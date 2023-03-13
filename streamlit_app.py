eadbe46cbf8818c8800fe77e3b43546d43f3ba54
Sean Smart <117030053+Sean-Smart@users.noreply.github.com>
2023-03-13 14:43:33 +0000

    Update streamlit_app.py
    Added table output

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,8 +55,11 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchall()
 #streamlit.text("Greetings Earthling, you have made connection with the following:")
+#output
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
+
 
 #User input of fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
