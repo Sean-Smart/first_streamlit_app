3158d3ca033c449300ed7e9b4df42fd43522f40d
Sean Smart <117030053+Sean-Smart@users.noreply.github.com>
2023-03-13 12:55:51 +0000

    Update streamlit_app.py
    Edited embedded SQL query

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,10 +48,12 @@ streamlit.dataframe(fruityvice_normalized)
 #Adding snowflake connector functionality
 import snowflake.connector
 
-#
+#This allows you to execute SQL queries I think
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Greetings Earthling, you have made connection with the following:")
+#streamlit.text("Greetings Earthling, you have made connection with the following:")
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
