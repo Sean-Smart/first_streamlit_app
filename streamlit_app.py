e5075dd886f7900ab9233591b1bcfcbc67cea136
Sean Smart <117030053+Sean-Smart@users.noreply.github.com>
2023-03-13 13:26:55 +0000

    Update streamlit_app.py
    reformat

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,8 +52,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_cur.execute("SELECT * FROM fruit_load_list")
-my_cur.execute("INSERT INTO fruit_load_list values ('from streamlit')")
+my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
 #streamlit.text("Greetings Earthling, you have made connection with the following:")
 streamlit.text("The fruit load list contains:")
@@ -62,4 +61,7 @@ streamlit.text(my_data_row)
 #User input of fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+#New feature
+my_cur.execute("INSERT INTO fruit_load_list values ('from streamlit')")
 
