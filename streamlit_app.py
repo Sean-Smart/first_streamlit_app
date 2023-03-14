2a23ae3cd14cbbbfbb7e24281bcacf4356ae3abb
Sean Smart <117030053+Sean-Smart@users.noreply.github.com>
2023-03-14 09:19:10 +0000

    Update streamlit_app.py
    fixed indentation error

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,8 +78,8 @@ streamlit.text("The fruit load list contains:")
 #snowflake related functions:
 def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
-     my_cur.execute("SELECT * FROM fruit_load_list")
-     return my_cur.fetchall()
+         my_cur.execute("SELECT * FROM fruit_load_list")
+         return my_cur.fetchall()
     
  #Adda button to load the fruit
  if streamlit.button('Get fruit load list'):
